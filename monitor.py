f5004910fe02c5164ae7100fd0846720d5a946ed
kwkScream <spoonkicker@gmail.com>
2026-04-16 13:52:56 +0300

    feat: added script for sending telegram messages

--- monitor.py
+++ monitor.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import requests
 from dotenv import load_dotenv
@@ -7,6 +8,23 @@ load_dotenv()
 
 bot_token = os.getenv("TELEGRAM_TOKEN")
 url = os.getenv("TARGET_URL")
+chat_id = os.getenv("TELEGRAM_CHAT_ID")
+api_url = f"https://api.telegram.org/bot<{bot_token}>/sendMessage"
+text = "Сайт впав! 🚨"
+
+
+def send_telegram_message(token, chat_id, text):
+    try:
+        url = f"https://api.telegram.org/bot{token}/sendMessage"
+        payload = {
+            "chat_id": chat_id,
+            "text": text,
+        }
+        response = requests.post(url, data=payload)
+        return response.json()
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return False
 
 
 def check_website(url):
@@ -20,3 +38,13 @@ def check_website(url):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
         return False
+
+
+if __name__ == "__main__":
+    while True:
+        if check_website(url):
+            pass
+        else:
+            send_telegram_message(bot_token, chat_id, text)
+        time.sleep(3)
+        print("checked")
